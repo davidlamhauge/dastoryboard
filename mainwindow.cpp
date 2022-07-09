d69416f4bf710b4aa425ba9fffd3ba12c591358a
David Lamhauge <davidlamhauge@gmail.com>
2022-07-09 22:34:11 +0200

    Saving, loading and changing color works

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -5,6 +5,7 @@
 #include <QScreen>
 #include <QDebug>
 #include <QMessageBox>
+#include <QFileDialog>
 #include <QDir>
 #include <QListWidgetItem>
 
@@ -16,18 +17,21 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
 
+    init();
+
     // position where we left it
-    QSize scr = QGuiApplication::primaryScreen()->availableSize();
+//    QSize scr = QGuiApplication::primaryScreen()->availableSize();
     QSettings settings("TeamLamhauge", "daStoryboard");
     resize(settings.value("winSize", QSize(1040, 780)).toSize());
-    move(settings.value("winPos", QPoint(scr.width()/2 - 1040/2, scr.height()/2 - 780/2)).toPoint());
-    qDebug() << "scr: " << scr << " size: " << size() << " pos: " << pos();
-
-    init();
+//    move(settings.value("winPos", QPoint(scr.width()/2 - 1040/2, scr.height()/2 - 780/2)).toPoint());
+    move(settings.value("winPos", QPoint(520, 390)).toPoint());
 
     connect(ui->btnExit, &QPushButton::clicked, this, &MainWindow::close);
     connect(ui->btnLoad, &QPushButton::clicked, this, &MainWindow::setupProject);
     connect(ui->btnResetPal, &QPushButton::clicked, this, &MainWindow::resetPalette);
+    connect(ui->btnSavePal, &QPushButton::clicked, this, &MainWindow::savePalette);
+    connect(ui->btnLoadPal, &QPushButton::clicked, this, &MainWindow::loadPalette);
+    connect(ui->lwPalette, &QListWidget::itemChanged, this, &MainWindow::onItemChanged);
 
     ui->gvSketchPad->setEnabled(false);
     ui->btnAddStoryboard->setEnabled(false);
@@ -53,6 +57,54 @@ bool MainWindow::eventFilter(QObject *obj, QEvent *e)
 
 void MainWindow::init()
 {
+    mOrgPalette << mLIGHTBLUE << mLIGHTGREEN << mLIGHTRED << mLIGHTYELLOW << mLIGHTBROWN
+           << mLIGHTPURPLE << mWHITE << mBLACK << mLIGHTGRAY << mDARKGRAY;
+    mCurPalette = mOrgPalette;
+    mActivePaletteList = mPaletteList;
+
+    QSettings settings("TeamLamhauge", "daStoryboard");
+    ui->lwPalette->clear();
+    QListWidgetItem* item;
+    for (int i = 0; i < 10; i++)
+    {
+        QString s = settings.value("palette/" + QString::number(i), "").toString();
+        item = new QListWidgetItem();
+        item->setFlags(item->flags() | Qt::ItemIsEditable);
+        if (s == "")
+        {
+            item->setText(mPaletteList.at(i));
+            item->setBackground(QBrush(mOrgPalette.at(i)));
+            int gCol = qGray(item->background().color().red(), item->background().color().green(), item->background().color().blue());
+            if (gCol > 127)
+                item->setForeground(QBrush(Qt::black));
+            else
+                item->setForeground(QBrush(Qt::white));
+            ui->lwPalette->addItem(item);
+            settings.setValue("palette/" + QString::number(i), QString(QString::number(mOrgPalette.at(i).red()) + "," +
+                                                                       QString::number(mOrgPalette.at(i).green()) + "," +
+                                                                       QString::number(mOrgPalette.at(i).blue()) + "," +
+                                                                       mPaletteList.at(i)));
+        }
+        else
+        {
+            QStringList list = s.split(",");
+            item->setText(list.at(3));
+            QColor color = QColor(list.at(0).toInt(), list.at(1).toInt(), list.at(2).toInt());
+            item->setBackground(QBrush(color));
+            int gCol = qGray(item->background().color().red(), item->background().color().green(), item->background().color().blue());
+            if (gCol > 127)
+                item->setForeground(QBrush(Qt::black));
+            else
+                item->setForeground(QBrush(Qt::white));
+            ui->lwPalette->addItem(item);
+            settings.setValue("palette/" + QString::number(i),  QString(QString::number(color.red()) + "," +
+                                                                        QString::number(color.green()) + "," +
+                                                                        QString::number(color.blue()) + "," +
+                                                                        mPaletteList.at(i)));
+            mActivePaletteList.replace(i, list.at(3));
+        }
+
+    }
 }
 
 void MainWindow::setupProject()
@@ -66,6 +118,7 @@ void MainWindow::setupProject()
     QStringList a = mActiveStoryboardFull.split("/");
     mActiveProject = a.at(a.size() - 2);
     mActiveStoryboard = a.at(a.size() - 1);
+    QDir(mActiveProjectFull).mkdir("misc");
 
     if (settings.value("project").toString().isEmpty() ||
             settings.value("scene").toString().isEmpty() ||
@@ -89,25 +142,98 @@ void MainWindow::setupProject()
 
 void MainWindow::resetPalette()
 {
-    QList<QColor> colors;
-    colors << mLIGHTBLUE << mLIGHTGREEN << mLIGHTRED << mLIGHTYELLOW << mLIGHTBROWN
-           << mLIGHTPURPLE << mWHITE << mBLACK << mLIGHTGRAY << mDARKGRAY;
-    QStringList txt;
-    txt << tr("Lighth blue") << tr("Lighth green") << tr("Lighth red") << tr("Lighth yellow") << tr("Lighth brown")
-           << tr("Lighth purple") << tr("White") << tr("Black") << tr("Lighth gray") << tr("Dark gray");
     QListWidgetItem* item;
     ui->lwPalette->clear();
     for (int i = 0; i < 10; i++)
     {
-        item = new QListWidgetItem(txt.at(i));
-        item->setBackground(QBrush(colors.at(i)));
-        int gCol = qGray(colors.at(i).red(), colors.at(i).green(), colors.at(i).blue());
+        item = new QListWidgetItem(mPaletteList.at(i));
+        item->setFlags(item->flags() | Qt::ItemIsEditable);
+        item->setBackground(QBrush(mOrgPalette.at(i)));
+        int gCol = qGray(mOrgPalette.at(i).red(), mOrgPalette.at(i).green(), mOrgPalette.at(i).blue());
         if (gCol > 127)
             item->setForeground(QBrush(Qt::black));
         else
             item->setForeground(QBrush(Qt::white));
         ui->lwPalette->addItem(item);
     }
+}
+
+void MainWindow::savePalette()
+{
+    QString filename = QFileDialog::getSaveFileName(this,
+                                                    tr("Save palette as"),
+                                                    "",
+                                                    tr("Palette files (*.dpal)"));
+    if (filename.isEmpty())
+        return;
+    else
+    {
+        QFile file(filename);
+        if (!file.open(QIODevice::WriteOnly | QIODevice::Text))
+            return;
+        QListWidgetItem* item;
+        QTextStream out(&file);
+        for (int i = 0; i < 10; i++)
+        {
+            item = ui->lwPalette->item(i);
+            QColor color = item->background().color();
+            out << QString(QString::number(color.red()) + "," +
+                           QString::number(color.green()) + "," +
+                           QString::number(color.blue()) + "," +
+                           mActivePaletteList.at(i) + "\n");
+        }
+    }
+}
+
+void MainWindow::loadPalette()
+{
+    QString filename = QFileDialog::getOpenFileName(this,
+                                                    tr("Open palette"),
+                                                    "",
+                                                    tr("Palette files (*.dpal)"));
+    if (filename.isEmpty())
+        return;
+
+    QFile file(filename);
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
+        return;
+
+    QTextStream in(&file);
+    QSettings settings("TeamLamhauge", "daStoryboard");
+    int i = 0;
+    while (i < 10)
+    {
+        QListWidgetItem* item = ui->lwPalette->takeItem(i);
+        QString line = in.readLine();
+        QStringList list = line.split(",");
+        QColor color = QColor(list.at(0).toInt(), list.at(1).toInt(), list.at(2).toInt());
+        item->setBackground(QBrush(color));
+        int gCol = qGray(item->background().color().red(), item->background().color().green(), item->background().color().blue());
+        if (gCol > 127)
+            item->setForeground(QBrush(Qt::black));
+        else
+            item->setForeground(QBrush(Qt::white));
+        item->setText(list.at(3));
+        ui->lwPalette->insertItem(i, item);
+        settings.setValue("palette/" + QString::number(i),  QString(QString::number(color.red()) + "," +
+                                                                    QString::number(color.green()) + "," +
+                                                                    QString::number(color.blue()) + "," +
+                                                                    item->text()));
+        mActivePaletteList.replace(i, list.at(3));
+        i++;
+    }
+}
+
+void MainWindow::onItemChanged(QListWidgetItem *item)
+{
+    int row = ui->lwPalette->currentRow();
+    mActivePaletteList.replace(row, item->text());
+    QSettings settings("TeamLamhauge", "daStoryboard");
+    QColor color = item->background().color();
+    settings.setValue("palette/" + QString::number(row),  QString(QString::number(color.red()) + "," +
+                                                                  QString::number(color.green()) + "," +
+                                                                  QString::number(color.blue()) + "," +
+                                                                  mActivePaletteList.at(row)));
 }
 
 void MainWindow::loadScene(QString scene)
--- mainwindow.h
+++ mainwindow.h
@@ -1,6 +1,7 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include "qlistwidget.h"
 #include <QMainWindow>
 
 namespace Ui {
@@ -13,7 +14,6 @@ class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
-
 public:
      explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
@@ -25,7 +25,12 @@ private:
 
     void init();
     void setupProject();
+
     void resetPalette();
+    void savePalette();
+    void loadPalette();
+    void onItemChanged(QListWidgetItem* item);
+
     void loadScene(QString scene);
 
 
@@ -34,6 +39,7 @@ private:
     QString mActiveStoryboardFull = "";
     QString mActiveProject = "";
     QString mActiveStoryboard = "";
+    QString mMiscFolderFull = "";
     int mActiveStoryboardFrames = 0;
     int mActiveStoryboardPads = 0;
     QVector<QPixmap> mPixmaps;
@@ -42,13 +48,19 @@ private:
     const QColor mLIGHTBLUE = QColor(160, 215, 255);
     const QColor mLIGHTGREEN = QColor(135, 255, 145);
     const QColor mLIGHTRED = QColor(240, 180, 180);
-    const QColor mLIGHTYELLOW = QColor(255, 245, 116);
+    const QColor mLIGHTYELLOW = QColor(245, 245, 125);
     const QColor mLIGHTBROWN = QColor(200, 115, 50);
     const QColor mLIGHTPURPLE = QColor(240, 155, 240);
     const QColor mWHITE = QColor(255, 255, 255);
     const QColor mBLACK = QColor(10, 10, 10);
     const QColor mLIGHTGRAY  = QColor(220, 220, 220);
     const QColor mDARKGRAY = QColor(150, 150, 150);
+    const QStringList mPaletteList = QStringList() << tr("Light blue") << tr("Light green") << tr("Light red") << tr("Light yellow") << tr("Light brown")
+                                      << tr("Light purple") << tr("White") << tr("Black") << tr("Light gray") << tr("Dark gray");
+    QStringList mActivePaletteList;
+    QList<QColor> mOrgPalette;
+    QList<QColor> mCurPalette;
+
 
     StartupMenu* mStartupMenu = nullptr;
 };
